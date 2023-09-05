7e2d1dd183111189c05ee31cf235afda707fac01
ap-sullivan <andrew_sullivan1984@yahoo.co.uk>
2023-09-05 21:41:49 +0100

    complete index and gallery site with hamburger menu

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// mobile navigation
+
+
+
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNav.addEventListener('click', function() {
+  headerEl.classList.toggle('nav-open')
+  
+})
+
