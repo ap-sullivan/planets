7c3d67317d131996b260c10a1526cd66ad06200a
ap-sullivan <andrew_sullivan1984@yahoo.co.uk>
2023-09-06 21:44:54 +0100

    create sticky nav

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,5 @@
 // mobile navigation
 
-
-
 const btnNav = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
@@ -9,4 +7,28 @@ btnNav.addEventListener('click', function() {
   headerEl.classList.toggle('nav-open')
   
 })
+
+const sectionHeroEl = document.querySelector(".hero")
+
+const obs = new IntersectionObserver(function(entries){
+const ent = entries[0];
+
+if (ent.isIntersecting === false){
+  document.body.classList.add('sticky')
+}
+
+if (ent.isIntersecting === true){
+  document.body.classList.remove('sticky')
+}
+
+},
+
+{
+  // in the viewport
+  root: null,
+  threshhold: 0,
+  rootMargin: '-80px' 
+
+})
+obs.observe(sectionHeroEl)
 
